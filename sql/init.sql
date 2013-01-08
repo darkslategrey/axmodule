a7fe79e455e256c56c947a71790e884b1c557491
grego <greg.faruch@gmail.com>
2013-01-08 18:45:34 +0100

    add type of event where creating a new one. DB constante

--- sql/init.sql
+++ sql/init.sql
@@ -4,3 +4,11 @@ DELETE FROM llx_const WHERE name = 'CONTACT_USE_COMPANY_ADDRESS' AND entity = 1;
 INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('CONTACT_USE_COMPANY_ADDRESS', '1','chaine',1,'Auxiliae',1);
 
 
+-- Add type of rdv when creating new event
+DELETE FROM llx_const WHERE name = 'AGENDA_USE_EVENT_TYPE' AND entity = 1;
+INSERT INTO llx_const(name,value,type,visible,note,entity) VALUES ('AGENDA_USE_EVENT_TYPE', '1','chaine',1,'',1);
+
+
+
+
+
